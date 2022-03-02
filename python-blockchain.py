7530abe174d481fc953d50c51824fd1ab772c297
StroeAdrianFF <stroeadrian85@yahoo.com>
2022-03-02 14:24:47 +0200

    learning some more

--- python-blockchain.py
+++ python-blockchain.py
@@ -1,10 +1,27 @@
-blockchain = [[1]]
+# initializing blockchain list
+blockchain = []
 
-def add_values(amount):
-    blockchain.append([blockchain[-1], amount])
-    print(blockchain)
 
-add_values(2)
-add_values(5)
-add_values(8)
-add_values(1.3)
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
+def add_values(amount, last_value=[1]):
+    blockchain.append([last_value, amount])
+
+
+def get_user_input():
+    return float(input('Amount to be transacted: '))
+
+
+transaction_amount = get_user_input()
+add_values(transaction_amount)
+
+transaction_amount = float(input('Amount to be transacted: '))
+add_values(last_value=get_last_blockchain_value(), amount=transaction_amount)
+
+transaction_amount = float(input('Amount to be transacted: '))
+add_values(transaction_amount, get_last_blockchain_value())
+
+
+print(blockchain)
