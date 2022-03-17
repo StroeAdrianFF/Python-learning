3ad445d0d9c0d140b4463391313fcd828b59df7f
StroeAdrianFF <stroeadrian85@yahoo.com>
2022-03-17 19:40:40 +0200

    updates

--- python-blockchain.py
+++ python-blockchain.py
@@ -1,5 +1,9 @@
-from rfc3986 import is_valid_uri
-import functools
+from functools import reduce
+import hashlib as hashing
+from collections import OrderedDict
+
+
+from  hash_util import  hash_string256, hash_block
 
 MINING_REWARD = 10
 
@@ -7,7 +11,8 @@ MINING_REWARD = 10
 genesis_block = {
     'previous_hash': '',
     'index': 0,
-    'transactions': []
+    'transactions': [],
+    'proof': 100
 }
 blockchain = [genesis_block]
 open_transactions = []
@@ -22,11 +27,13 @@ def get_last_blockchain_value():
 
 
 def add_transaction(recipient, sender=owner, amount=1.0):
-    transaction = {
-        'sender': sender,
-        'recipient': recipient,
-        'amount': amount
-    }
+#    transaction = {
+#        'sender': sender,
+#        'recipient': recipient,
+#        'amount': amount
+#    } 
+  
+    transaction = OrderedDict([('sender', sender),('recipient', recipient),('amount', amount)])
     if verify_transaction(transaction):     
         open_transactions.append(transaction)
         participants.add(sender)  # duplicates will be ignored
@@ -34,24 +41,6 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         return True
     return False
 
-
-def mine_block():
-    last_block = blockchain[-1]
-    hashed_block = hash_block(last_block)
-    reward_transaction = {
-        'sender': 'MINING',
-        'recipient': owner,
-        'amount': MINING_REWARD
-    }
-    copied_transactions = open_transactions[:] #copy open transactions from start to end
-    copied_transactions.append(reward_transaction)
-    block = {
-        'previous_hash': hashed_block,
-        'index': len(blockchain),
-        'transactions': copied_transactions #ensure that we manipulate a list of transaction which is managed locally not globally
-    }
-    blockchain.append(block)
-    return True
 
 
 def get_trans_value():
@@ -71,8 +60,42 @@ def print_blockchain():
         print(block)
 
 
-def hash_block(block):
-    return '-'.join([str(block[key]) for key in block])
+def valid_proof(transactions, last_hash, proof_number):
+    guess = (str(transactions)+ str(last_hash) + str(proof_number)).encode()
+    guess_hash = hash_string256(guess)  #get valid string
+    print(guess_hash)
+    return guess_hash[0:2] == '00' #check if hash is valid only if it starts with 2 zeros
+
+
+def pow():
+    last_block = blockchain[-1]
+    last_hash = hash_block(last_block)
+    proof = 0
+    while not valid_proof(open_transactions, last_hash, proof):
+        proof += 1
+    return proof
+
+def mine_block():
+    last_block = blockchain[-1]
+    hashed_block = hash_block(last_block)
+    proof = pow()
+#    reward_transaction = {
+#        'sender': 'MINING',
+#        'recipient': owner,
+#        'amount': MINING_REWARD
+#    }
+    reward_transaction = OrderedDict([('sender', 'MINING'),('recipient', owner),('amount', MINING_REWARD)])
+    copied_transactions = open_transactions[:] #copy open transactions from start to end
+    copied_transactions.append(reward_transaction)
+    block = {
+        'previous_hash': hashed_block,
+        'index': len(blockchain),
+        'transactions': copied_transactions, #ensure that we manipulate a list of transaction which is managed locally not globally
+        'proof': proof
+    }
+    blockchain.append(block)
+    return True
+
 
 
 def get_balances(participant):
@@ -80,24 +103,27 @@ def get_balances(participant):
     trans_recipient = [[transactions['amount'] for transactions in block['transactions'] if transactions['recipient'] == participant] for block in blockchain]
     open_trans_sender = [transactions['amount'] for transactions in open_transactions if transactions['sender'] == participant]
     trans_sender.append(open_trans_sender)
-    amount_sent = functools.reduce(lambda trans_sum,trans_amount:trans_sum+sum(trans_amount) if len(trans_amount)>0 else trans_sum + 0, trans_sender, 0)
-    amount_received = functools.reduce(lambda trans_sum, trans_amount: trans_sum+sum(trans_amount) if len(trans_amount)>0 else trans_sum + 0, trans_recipient, 0)
+    amount_sent = reduce(lambda trans_sum,trans_amount:trans_sum+sum(trans_amount) if len(trans_amount)>0 else trans_sum + 0, trans_sender, 0)
+    amount_received = reduce(lambda trans_sum, trans_amount: trans_sum+sum(trans_amount) if len(trans_amount)>0 else trans_sum + 0, trans_recipient, 0)
    
     return amount_received - amount_sent
 
 
 def verify_chain():
-    # enumarte contains index of element and the element as a tuple
+    # enumerate contains index of element and the element as a tuple
     for (index, block) in enumerate(blockchain):
         if index == 0:
             continue
         if block['previous_hash'] != hash_block(blockchain[index-1]):
             return False
+        if not valid_proof(block['transactions'][:-1],block['previous_hash'], block['proof']):
+            print('Proof of work is invalid')
+            return False
     return True
 
 
 def verify_transaction(transaction):
-    sender_balance  =get_balances(transaction['sender'])
+    sender_balance = get_balances(transaction['sender'])
     return sender_balance >= transaction['amount']
     
 
